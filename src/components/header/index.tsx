7be16f3477aeaf8957f6d90d5bf1e38774134763
MariaJulia <mariajuliadev@gmail.com>
2025-04-08 09:50:55 -0300

    Ajuste na responsividade do header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,7 +3,7 @@ import { IoMdCart } from "react-icons/io";
 
 export function Header() {
     return (
-        <header className="w-full flex px-1 bg-white border-b border-gray-300 h-16 sticky top-0 z-10">
+        <header className=" w-full flex px-1 bg-white border-b border-gray-300 h-16 sticky top-0 z-10">
 
             <nav className="w-full max-w-yxl flex items-center justify-between px-5 mx-auto">
                 <a href="#start"
@@ -11,18 +11,18 @@ export function Header() {
                     Pet Love
                 </a>
 
-                <div className="sm:text-xl sm:gap-5 md:text-xl flex items-center gap-2 select-none text-[#121212] font-medium text-base">
+                <div className="hidden sm:text-xl sm:gap-5 md:text-xl sm:flex items-center gap-2 select-none text-[#121212] font-medium text-base">
                     <a href="#featuredProducts" className="transition-all duration-200 ease-in-out hover:scale-105">Produtos</a>
                     <a href="#ourServices" className="transition-all duration-200 ease-in-out hover:scale-105">Serviços</a>
                     <a href="#about" className="transition-all duration-200 ease-in-out hover:scale-105">Sobre</a>
                     <a href="#clients" className="transition-all duration-200 ease-in-out hover:scale-105">Depoimentos</a>
                     <a href="#contact" className="transition-all duration-200 ease-in-out hover:scale-105">Contato</a>
-
-                    <Link to="/cart" className="sm:ml-8 relative">
-                        <IoMdCart size={26} color="#121212" />
-                        <span className=" bg-[#F66D21] rounded-full w-5 h-5 flex items-center justify-center text-white text-base absolute -right-2 -top-2">2</span>
-                    </Link>
                 </div>
+
+                <Link to="/cart" className="sm:ml-8 relative">
+                    <IoMdCart size={26} color="#121212" />
+                    <span className=" bg-[#F66D21] rounded-full w-5 h-5 flex items-center justify-center text-white text-base absolute -right-2 -top-2">2</span>
+                </Link>
             </nav>
 
         </header>
